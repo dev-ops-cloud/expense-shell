b78bb9e19b4055e8a70d9613035f0b659c0b9819
Pallavi C <info@joindevops.com>
2025-01-04 23:31:16 +0530

    expense-shell

--- backend.sh
+++ backend.sh
@@ -59,6 +59,7 @@ curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expen
 VALIDATE $? "Downloading backend"
 
 cd /app
+rm -rf /app/*
 
 unzip /tmp/backend.zip &>>$LOG_FILE_NAME
 VALIDATE $? "unzip backend"
@@ -82,5 +83,5 @@ VALIDATE $? "Daemon reload"
 systemctl enable backend &>>$LOG_FILE_NAME
 VALIDATE $? "Enabling backend"
 
-systemctl start backend &>>$LOG_FILE_NAME
-VALIDATE $? "Starting backend"
+systemctl restart backend &>>$LOG_FILE_NAME
+VALIDATE $? "restarting backend"
